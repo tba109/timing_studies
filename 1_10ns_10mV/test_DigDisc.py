31c818c0e58a532374fcebc17c78a1ced8a2a623
Tyler Anderson <tba109@psu.edu>
2018-12-12 22:39:59 -0500

    More work

--- 1_10ns_10mV/test_DigDisc.py
+++ 1_10ns_10mV/test_DigDisc.py
@@ -12,7 +12,7 @@ from ROOT import TH1F
 # Waveform parameters
 # fsps = np.array([1.])
 # fsps = np.arange(1.,11.,0.5)
-fsps = [100.]
+fsps = [10.]
 Nwaves = 10000
 v0 = 0.
 # v1 = np.arange(10.,110.,10.)
@@ -24,7 +24,7 @@ ton = 10.
 tfall = 10.
 sigma_v = np.arange(0.1,1,0.1)
 # sigma_v = np.arange(0.001,.01,0.001)
-# sigma_v = np.array([.1])
+# sigma_v = np.array([0.01])
 vthr = 5.
 
 ddc = DigDiscClass.DigDiscClass(vthr)
@@ -35,8 +35,8 @@ fout = open('out.txt','w')
 tdisc_method = 'ddc'
 # tdisc_method = 'dic'
 
-plot_wv = True
-# plot_wv = False
+# plot_wv = True
+plot_wv = False
 
 # Fit the curves
 def f(x,m,b): 
@@ -48,7 +48,7 @@ for n in range(len(fsps)):
         for k in range(len(trise)):
             for j in range(len(sigma_v)):
                 tdisc = np.zeros(Nwaves)
-                h1 = TH1F("h1","",1000,4.9,5.1)
+                # h1 = TH1F("h1","",1000,4.9,5.1)
                 for i in range(Nwaves): 
                     # Horizontal array
                     wfn = WaveFormNoiser.WaveFormNoiser(sigma_v[j],fsps[n])
@@ -81,16 +81,18 @@ for n in range(len(fsps)):
                 sigma = np.std(tdisc)
                 sigma_t[j] = sigma
                 print 'fsps = %f, v1 = %f, trise = %f, sigma_v = %f, sigma_t = %f' % (fsps[n],v1[m],trise[k],sigma_v[j],sigma)
-                plt.hist(tdisc,100)
-                plt.show()
+                # plt.hist(tdisc,100)
+                # plt.show()
                 # h1.Draw()
                 # raw_input()
                 # h1.Delete()
-            popt,pcov = curve_fit(f,sigma_v[3:-1],sigma_t[3:-1])
+            # popt,pcov = curve_fit(f,sigma_v[3:-1],sigma_t[3:-1])
+            popt,pcov = curve_fit(f,sigma_v,sigma_t)
             strout = '%f,%f,%f,%f,%f\n' % (fsps[n],v1[m],trise[k],popt[0],popt[1])
             print strout
             plt.plot(sigma_v,sigma_t,'o-')
-            plt.plot(sigma_v[3:-1],f(sigma_v[3:-1],popt[0],popt[1]),'r-')
+            # plt.plot(sigma_v[3:-1],f(sigma_v[3:-1],popt[0],popt[1]),'r-')
+            plt.plot(sigma_v,f(sigma_v,popt[0],popt[1]),'r-')
             plt.show()
             fout.write(strout)
 fout.close()
